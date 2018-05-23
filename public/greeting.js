4231f0b4f81ab0b7b41fa185f94b6c7de52e9c37
GRIFF\Nicholas <nicholas.s.griffen@gmail.com>
2018-05-23 03:49:02 -0700

    style revision

--- public/greeting.js
+++ public/greeting.js
@@ -1,43 +1,40 @@
-const rawDate = new Date();
-const mmDD = [rawDate.getMonth() + 1, rawDate.getDate()];
+const rawDate, mmDD, days, commonday, months, monthName, dateSuffs, sadWords, fname, xmlhttp, url;
 
-//arrays to map Date method return values to commonly used names 
-const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-const dateSuffs = [ 0, "1st","2nd","3rd","4th","5th","6th","7th","8th","9th","10th","11th","12th","13th","14th","15th","16th","17th","18th","19th","20th","21st","22nd","23rd",
-	"24th","25th","26th","27th","28th","29th","30th","31st"];
-//filter out negativity / sadness in the trivia
-const sadWords = ["dead","died","terrorist","bomb","bomber","murder","death","bombing","killer","rape","kidnap","kidnapper","kidnapped","execution","killed","disaster","terrorism","terror","hijacked","attack","war","rebel", "massacre","killing"];
-//get the day of the week as a number, use that as an index to find our day of the week name from the days array
-const monthName = months[mmDD[0]];
-const commonDay = days[rawDate.getDay()];
+days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
-//add 0 to the front of Date function return values if necessary, return name
-//to be used in the path for the xml call to our data folder
+months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
-const fName = mmDD.map(index => index < 10 ? `0${index}` : `${index}`).join("-");
+dateSuffs = [ 0, "1st","2nd","3rd","4th","5th","6th","7th","8th","9th","10th","11th","12th","13th","14th","15th","16th","17th","18th","19th","20th","21st","22nd","23rd","24th","25th","26th","27th","28th","29th","30th","31st"];
 
-//make xml request to grab our JSON file for today's date in history
-var xmlhttp = new XMLHttpRequest();
-var url = `/data/${fName}.json`;
+rawDate = new Date();
 
+mmDD = [rawDate.getMonth() + 1, rawDate.getDate()];
 
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
-
-//when the request is done with status success, parse our file
-xmlhttp.onreadystatechange = function() {
-	if (this.readyState == 4 && this.status == 200) {
-		var triv = JSON.parse(this.response);
-		eventOrBorn(triv);
-	}
-};
-
-//load our greeting in the view with the name of the day of the week
+commonDay = days[rawDate.getDay()];
 document.getElementById("greeting").innerHTML = `Happy ${commonDay}!!!`;
 
-//select a random event from the file and display its contents in our view
-function eventOrBorn(triv) {
+monthName = months[mmDD[0]];
+fName = mmDD.map(index => index < 10 ? `0${index}` : `${index}`).join("-");
+
+function randElement(array) {
+	let randIndex = Math.floor(Math.random() * (array.length - 1));
+	let testTrivium = array[randIndex];
+	return testTrivium;
+}
+
+function displayTrivia(trivia = [{}], heading) {
+	let trivium = randElement(trivia);
+	document.getElementById("triviaheader").innerHTML = heading;
+	document.getElementById("trivia").innerHTML = trivium.year + ": " + trivium.text;
+}
+
+function triviaController(trivia, heading) {
+	(function cycleTriv() {
+		setInterval(function() {displayTrivia(trivia, heading)}, 12000);
+	})();
+}
+
+function eventOrBirth(triv) {
 	if (commonDay == "Monday" || commonDay == "Wednesday" || commonDay == "Friday") {
 		triviaController(triv.data.Events, "");
 	} else {
@@ -45,25 +42,20 @@ function eventOrBorn(triv) {
 	}
 }
 
-function triviaController(trivia, heading) {
-//collect our element of trivia which remember is an array, in a variable
-	(function cycleTriv() {
-		setInterval(function() {displayTrivia(trivia, heading)}, 12000);
-	})();
-}
-function displayTrivia(trivia, heading) {
-	//returns a random element of our dataset as an array
-	trivium = randElement(trivia);
-	//insert elements of our trivium in the HTML of our view
-	document.getElementById("triviaheader").innerHTML = heading;
-	document.getElementById("trivia").innerHTML = trivium.year + ": " + trivium.text;
-}
-function randElement(array) {
-	/*generate random integer as index on our array
-	length returns the number of elements in our array, but our array is zero-indexed so 
-	we need to remove one from the length to get our upper limit for indexing*/
-	let randIndex = Math.floor(Math.random() * (array.length - 1));
-	//retrieve random element from array
-	let testTrivium = array[randIndex];
-	return testTrivium;
-}
+xmlhttp = new XMLHttpRequest();
+file = `/data/${fName}.json`;
+
+xmlhttp.open("GET", file, true);
+xmlhttp.send();
+
+xmlhttp.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+		let triv = JSON.parse(this.response);
+		eventOrBirth(triv);
+	}
+};
+
+
+
+
+
