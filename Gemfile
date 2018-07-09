86a390d93ed098400b3c329edb93e0eeace3f9e0
griff <nicholas.s.griffen@gmail.com>
2018-07-08 22:01:50 -0700

    convert backend to express

--- Gemfile
+++
@@ -1,5 +0,0 @@
-ruby '2.2.5'
-source 'https://rubygems.org'
-
-gem 'sinatra'
-gem 'nokogiri', '~> 1.8.1'
--- jan.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const jan = express();
-
-jan.get('/', (req, res) => 
-    res.sendFile(`${__dirname}/views/welcome.html`)
-);
-jan.listen(3000, () => 
-    console.log('The Jan is listening')
-);
